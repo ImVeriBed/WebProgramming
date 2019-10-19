e2773d63537d3c346832d7cd159c7f213cd2ab9e
Koptsev G.A <44699659+ImVeriBed@users.noreply.github.com>
2019-10-19 17:34:42 +0300

    Переезд на php

--- index.php
+++ index.php
@@ -1,44 +1,44 @@
-﻿<!DOCTYPE html>
-
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+﻿<?php
+echo "<link href='css/bootstrap.css' rel='stylesheet'/>";
+echo "<link href='css/mystyle_index.css' rel='stylesheet'/>";
+echo "<html>
 
 <head>
-    <meta charset="utf-8" />
+	<meta charset='utf-8' />
     <title>Главная страница</title>
-    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
-    <link href="css/mystyle_index.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
-    <script src="js/bootstrap.js"></script>
+    <script src='js/bootstrap.js'></script>
 
-    <div class="divflex">
-        <div class="logo"></div>
+    <div class='divflex'>
+        <div class='logo'></div>
         <!-- Верхняя панель -->
-        <form class="formHeader">
-            <div class="form-inline">
-                <div class="form-group">
-                    <label for="login">Логин:</label>
-                    <input class="form-control" id="login">
+        <form class='formHeader'>
+            <div class='form-inline'>
+                <div class='form-group'>
+                    <label for='login'>Логин:</label>
+                    <input class='form-control' id='login'>
                 </div>
-                <div class="form-group">
-                    <label for="pwd">Пароль:</label>
-                    <input type="password" class="form-control" id="pwd">
+                <div class='form-group'>
+                    <label for='pwd'>Пароль:</label>
+                    <input type='password' class='form-control' id='pwd'>
                 </div>
-                <div class="checkbox">
-                    <label><input type="checkbox"> Запомнить меня?</label>
+                <div class='checkbox'>
+                    <label><input type='checkbox'> Запомнить меня?</label>
                 </div>
-                <button type="submit" class="btn btn-default">Войти</button>
+                <button type='submit' class='btn btn-default'>Войти</button>
             </div>
         </form>
         <!-- Верхняя панель -->
         <!-- Основной контент -->
-        <form class="mainform">
-            <div class="btn-group-vertical">
-                <a class="btn btn-default">Главная страница</a>
-                <a href="lab_rab1.html" class="btn btn-default">Лабораторная №1</a>
+        <form class='mainform'>
+            <div class='btn-group-vertical'>
+                <a class='btn btn-default'>Главная страница</a>
+                <a href='lab_rab1.html' class='btn btn-default'>Лабораторная №1</a>
+				<a href='lab_rab1.html' class='btn btn-default'>Лабораторная №2</a>
             </div>
-            <p class="textFont">
+            <p class='textFont'>
                 Совмещение отдыха с познанием жизни, истории и культуры народов - одна из задач, которую в полной
                 мере способен решать туризм. ... Социальное значение туризма .Туризм как вид отдыха помогает
                 восстановить силы и трудоспособность человека и соответственно — психофизиологические ресурсы общества.
@@ -55,9 +55,9 @@
         </form>
         <!-- Основной контент -->
         <!-- Нижняя панель -->
-        <form class="formVault">
-            <div class="form-inline">
-                <p class="text-center">
+        <form class='formVault'>
+            <div class='form-inline'>
+                <p class='text-center'>
                     Рязань, 2019 г.
                 </p>
             </div>
@@ -67,4 +67,5 @@
 
 </body>
 
-</html>
+</html>";
+?>
